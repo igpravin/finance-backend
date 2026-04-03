e0ae2e2a0868cf0aac6cb0d865606ded62f944f0
Pravin Kumar <parveen100ps97@gmail.com>
2026-04-03 13:08:11 +0530

    Fix port issue for render

--- finance-backend/server.js
+++ finance-backend/server.js
@@ -1,10 +1,15 @@
-require("dotenv").config();   // ✅ ADD THIS
+require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/db");
 
+// Connect Database
 connectDB();
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running on port " + process.env.PORT);
+// Define PORT properly
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
